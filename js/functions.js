b61d19da7229a1d08aff5fe83cabc94e970eaa53
Pol Forns <victory407@gmail.com>
2018-07-21 21:17:47 +0200

    BS-0014 - work on blog shapes

--- js/functions.js
+++ js/functions.js
@@ -32,28 +32,50 @@ $(document).ready(
 );
 
 $(document).ready(function() {
-	if ($(window).width() < 1200) {
+	if ($(window).width() >= 1200) {
+		$('.triangleDescubrenos').css('border-left-width', '600px');
+	  $('.triangleDescubrenos').css('border-right-width', '600px');
+	  $('.quadratDescubrenos').css('height', 0);
+	} else {
 		$('.triangleDescubrenos').css('border-left-width', $(window).width() / 2);
 	  $('.triangleDescubrenos').css('border-right-width', $(window).width() / 2);
 	  $('.quadratDescubrenos').css('height', 700 - $(window).width());
 	  $('.triangleDescubrenos').css('border-top-width', 550 - $('.quadratDescubrenos').height());
+	}
+
+	if ($(window).width() <= 2000) {
+	  $('.trianglesBlog__dre').css('border-right-width', $(window).width() * 0.6);
+	  $('.trianglesBlog__dre').css('border-top-width', $(window).width() * 0.4);
+	  $('.trianglesBlog__dre').css('border-bottom-width', $(window).width() * 0.4);
+	  $('.trianglesBlog__esq').css('border-left-width', $(window).width() * 0.6);
+	  $('.trianglesBlog__esq').css('border-top-width', $(window).width() * 0.4);
+	  $('.trianglesBlog__esq').css('border-bottom-width', $(window).width() * 0.4);
+	  $('.trianglesBlog__esq').css('margin-top', -($(window).width() * 0.62));
 	} else {
-		$('.triangleDescubrenos').css('border-left-width', '600px');
-	  $('.triangleDescubrenos').css('border-right-width', '600px');
-	  $('.quadratDescubrenos').css('height', 0);
 	}
 });
 
 $(window).on('resize', function () {
-	if ($(window).width() < 1200) {
+	if ($(window).width() >= 1200) {
+		$('.triangleDescubrenos').css('border-left-width', '600px');
+	  $('.triangleDescubrenos').css('border-right-width', '600px');
+	  $('.quadratDescubrenos').css('height', 0);
+	} else {
 	  $('.triangleDescubrenos').css('border-left-width', $(window).width() / 2);
 	  $('.triangleDescubrenos').css('border-right-width', $(window).width() / 2);
 	  $('.quadratDescubrenos').css('height', 700 - $(window).width());
 	  $('.triangleDescubrenos').css('border-top-width', 550 - $('.quadratDescubrenos').height());
+	}
+
+	if ($(window).width() <= 2000) {
+	  $('.trianglesBlog__dre').css('border-right-width', $(window).width() * 0.6);
+	  $('.trianglesBlog__dre').css('border-top-width', $(window).width() * 0.4);
+	  $('.trianglesBlog__dre').css('border-bottom-width', $(window).width() * 0.4);
+	  $('.trianglesBlog__esq').css('border-left-width', $(window).width() * 0.6);
+	  $('.trianglesBlog__esq').css('border-top-width', $(window).width() * 0.4);
+	  $('.trianglesBlog__esq').css('border-bottom-width', $(window).width() * 0.4);
+	  $('.trianglesBlog__esq').css('margin-top', -($(window).width() * 0.62));
 	} else {
-		$('.triangleDescubrenos').css('border-left-width', '600px');
-	  $('.triangleDescubrenos').css('border-right-width', '600px');
-	  $('.quadratDescubrenos').css('height', 0);
 	}
 }).resize();
 
